f8ae281c08d30809f87eb0637b69a8201c2c3288
MASTERPOP-N <duopoc@gmail.com>
2024-08-23 15:46:12 +0700

    Fix index

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -8,8 +8,9 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('projects/', include('projects.urls')),
-    path('', include('users.urls')),
+    
+    path('', include('projects.urls')),
+    path('users/', include('users.urls')),
     path('api/', include('api.urls')),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
