d72de24a4f9db3e0332b82616afc4903f69d7b31
MASTERPOP-N <duopoc@gmail.com>
2024-08-23 15:12:45 +0700

    fix project urls

--- projects/urls.py
+++ projects/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('project', views.project, name='project')
+    path('project', views.project, name='project'),
     path('project/<str:pk>/', views.project, name="project"),
-
     path('create-project/', views.createProject, name="create-project"),
-
     path('update-project/<str:pk>/', views.updateProject, name="update-project"),
-
     path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
 ]
